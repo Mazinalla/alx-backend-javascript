37abbac791c342fcdcefdd656fa48c38b4935b5e
mazinalla <mazinelsadig8@gmail.com>
2024-10-03 03:20:37 +0300

    Scope const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  var task2 = false;
-  var task = true;
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
   }
+  var task = false;
+  var task2 = true;
   return [task, task2];
 }
