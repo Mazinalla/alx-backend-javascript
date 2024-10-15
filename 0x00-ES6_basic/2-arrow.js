f064844a012595002efdec3bffda78f38e6965dd
mazinalla <mazinelsadig8@gmail.com>
2024-10-15 14:18:02 +0300

    declered -> anonymous

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
-export default function () {
+export default () => {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   const self = this;
   this.addNeighborhood = function add(newNeighborhood) {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
-}
+};
