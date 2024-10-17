6d06a2665fe48a296e95da2af9eb8fc446cf0341
mazinalla <mazinelsadig8@gmail.com>
2024-10-18 01:35:11 +0300

    def-param

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
 export default function getSumOfHoods(initialNumber = undefined, expansion1989 = undefined, expansion2019 = undefined) {
-  if (expansion1989 === undefined && expansion2019 === undefined) return initialNumber + expansion1989 + expansion2019;
+  return initialNumber + expansion1989 + expansion2019;
 }
