846c93fe11578e17a905673705d8db8d6539383a
mazinalla <mazinelsadig8@gmail.com>
2024-11-13 07:23:14 +0300

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,14 @@
-export function getResponseFromAPI(){
-    return new Promise((resolve, reject) => {
-        // Simulate an API call with a delay
-        const success = true; // Change to `false` to test rejection
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API call with a delay
+    const success = true; // Change to `false` to test rejection
 
-        setTimeout(() => {
-            if (success) {
-                resolve("API call successful!");
-            } else {
-                reject("API call failed!");
-            }
-        }, 1000); // 1 second delay
-    });
+    setTimeout(() => {
+      if (success) {
+        resolve('API call successful!');
+      } else {
+        reject(Error('API call failed!'));
+      }
+    }, 1000); // 1 second delay
+  });
 }
